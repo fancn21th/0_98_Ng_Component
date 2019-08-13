377131269883e64396d8ce00a029791b38681185
alex.fan <alex.fan@okchem.com>
2019-08-13 16:21:24 +0800

    re-org

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { HomeModule } from "./home.module";
+import { HomeModule } from "./home/home.module";
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from "./home.component";
 
 @NgModule({
   declarations: [HomeComponent],
